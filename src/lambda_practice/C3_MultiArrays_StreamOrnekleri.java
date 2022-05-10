b8d7a42cb71d81fa71a6adf380104234810497d5
Meryem Akay <haceryol491@gmail.com>
2022-05-10 18:38:29 +0300

    10.05.2022

--- src/lambda_practice/C3_MultiArrays_StreamOrnekleri.java
+++ src/lambda_practice/C3_MultiArrays_StreamOrnekleri.java
@@ -1,5 +1,5 @@
 package lambda_practice;
-//multi arrayleri cevirirken flapMap kullaniriz yapilis sekli iki turludur
+//multi arrayleri cevirirken flatMap kullaniriz yapilis sekli iki turludur
 //1) lamda kullanarak
 // Arrays.stream(arr).flatMap(t->Arrays.stream(t))
 //2) method ile
@@ -17,22 +17,51 @@ public class C3_MultiArrays_StreamOrnekleri {
                 {"Portakal", "Cilek", "Limon"},
                 {"Havuc","Erik"}
         };
-
+        System.out.println("list(arr) = " + list(arr));
+        System.out.print("e ıle baslaynlar : ");
+        doubleYaz(arr);
+        System.out.println("eıleBaslayanYazListe(arr) = " + eıleBaslayanYazListe(arr));
+        kıleBıtenlereyıldız(arr);
 
     }
     // S1 : tum elemanlari list yapalim
-
+public static List<String>list(String[][] str){
+        return Arrays.stream(str).flatMap(t->Arrays.stream(t)).collect(Collectors.toList());
+}
 
 
     // S2: E ile baslayan elemanlari double (elmaelma) olarak yazdiralim
 
+    public static void doubleYaz(String[][] str){
+         Arrays.stream(str).
+                 flatMap(t->Arrays.stream(t)).
+                 filter(t->t.toLowerCase().startsWith("e")).
+                 map(t->t+t).
+                forEach(t->System.out.println(t +" "));
+    }
 
 
     // S3: E ile baslayan elemanlari liste olarak yazdiralim
+    public static List<String> eıleBaslayanYazListe(String[][] str){
+
+
+    return    Arrays.stream(str).
+    flatMap(t->Arrays.stream(t)).
+    filter(t->t.toLowerCase().startsWith("e")).
+            collect(Collectors.toList());
+
+}
 
 
 
     //S4 : k ile bitenlerin sonuna '*' ekleyelim
+public static void kıleBıtenlereyıldız(String [][] arr){
 
+        Arrays.stream(arr).
+                flatMap(t->Arrays.stream(t)).
+                filter(t->t.toLowerCase().startsWith("k")).
+                map(t->t+"*").
+                forEach(t->System.out.println(t + " "));
+}
 
 }
--- src/lambda_practice/C4_Method_Refarance_Int.java
+++ src/lambda_practice/C4_Method_Refarance_Int.java
@@ -15,19 +15,37 @@ public class C4_Method_Refarance_Int {
     public static void main(String[] args) {
         List<Integer> intL= Arrays.asList(-2,-8,-5,0,2,5,6,7,15,6,8);
 
-
+        tekYaz(intL);
+        System.out.println("***\n");
+        System.out.println("ciftlerıCarp(intL) = " + ciftlerıCarp(intL));
+        System.out.println("negatıflerınKareTop(intL) = " + negatıflerınKareTop(intL));
+        System.out.println("maxPoztfKup(intL) = " + maxPoztfKup(intL));
     }
     // S1: tekleri aralarinda bir bosluk birakarak yazdiralim
+public static void tekYaz(List<Integer> l){
+        l.stream().filter(Methods::tekMi).forEach(Methods::yazInteger);
 
+}
 
     // S2: ciftlein carpimini bulalim
+    public static Optional<Integer> ciftlerıCarp(List<Integer> l){
+       return l.stream().filter(Methods::ciftMi).
+               reduce(Math::multiplyExact); //yan yana sayıları carpar
+
+    }
 
 
     // S3: negatiflerin kare toplamlarini  bulalim
+    public static Optional<Integer> negatıflerınKareTop(List<Integer> l){
+        return l.stream().filter(Methods::negatifMi).map(Methods::kareBul).reduce(Methods::toplam);
 
+    }
 
-    // S4: poziflerin kuplerinden max yazdiralim
-
+    // S4: poziflerin kuplerinden max olanı yazdiralim
+    public static Optional<Integer> maxPoztfKup(List<Integer> l){
+        return l.stream().filter(Methods::pozitifMi).
+                map(Methods::kupBul).reduce(Math::max);
+    }
 
 
 }
